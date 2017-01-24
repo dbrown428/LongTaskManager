5ba049761734227ba51bd0ffdcebd764cdd07b57
D. Brown <dbrown428@gmail.com>
2017-01-23 17:58:45 -0800

    Backoff cleanup

--- src/BaseTwoExponentialBackoff.ts
+++ src/BaseTwoExponentialBackoff.ts
@@ -3,18 +3,21 @@ import {Backoff} from "./Backoff";
 export class BaseTwoExponentialBackoff implements Backoff {
 	private exponent: number;
 
+	public static withMultiplierAndMaximum(base: number, maximum: number): Backoff {
+		return new BaseTwoExponentialBackoff(base, maximum);
+	}
+
 	private constructor(private multiplier: number, private maximum: number) {
 		this.exponent = 0;
+		this.validate(multiplier, maximum);
+	}
 
+	private validate(multiplier: number, maximum: number) {
 		if (this.multiplier <= 0) {
 			throw new RangeError("The base value must be greater-than zero.");
 		} else if (this.maximum <= this.multiplier && this.maximum != 0) {
 			throw new RangeError("The maximum value must be greater than the base value.");
 		}
-	}
-
-	public static withMultiplierAndMaximum(base: number, maximum: number): Backoff {
-		return new BaseTwoExponentialBackoff(base, maximum);
 	}
 
 	public static withMultiplier(base: number): Backoff {
