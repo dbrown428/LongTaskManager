480911c222e07ead003f67186dd0550345c9f637
D. Brown <dbrown428@gmail.com>
2017-02-01 11:26:53 -0800

    Add now timestamp to the claim id

--- src/ClaimId.ts
+++ src/ClaimId.ts
@@ -1,3 +1,14 @@
 export class ClaimId {
-	constructor(readonly value: string) {}
+	readonly value: string;
+
+	public static withNowTimestamp(): ClaimId {
+		const date = Date.now();
+		const now = date.toString();
+		
+		return new ClaimId(now);
+	}
+	
+	private constructor(value: string) {
+		this.value = value;
+	}
 }
---
+++ tests/unit/ClaimId.spec.ts
@@ -0,0 +1,9 @@
+import {assert} from "chai";
+import {ClaimId} from "../../src/ClaimId";
+
+describe("ClaimId", () => {
+	it("Should generate a super precise timestamp for this moment.", () => {
+		const claimId = ClaimId.withNowTimestamp();
+		assert.lengthOf(claimId.value, 13);
+	});
+});
