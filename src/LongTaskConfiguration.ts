7e91130690726e104568a27e24f165f73b222eac
D. Brown <dbrown428@gmail.com>
2017-02-01 17:15:47 -0800

    Configuration values for long tasks

--- src/LongTaskConfiguration.ts
+++ src/LongTaskConfiguration.ts
@@ -1,4 +1,5 @@
 export interface LongTaskConfiguration {
-	cleanupDelay: number;
 	maximumConcurrency: number;
+	cleanupDelayInSeconds: number;
+	maximumProcessingTimeInSeconds: number;
 }
--- src/LongTaskConfigurationDevelopment.ts
+++ src/LongTaskConfigurationDevelopment.ts
@@ -1,6 +1,7 @@
 import {LongTaskConfiguration} from "./LongTaskConfiguration";
 
 export class LongTaskConfigurationDevelopment implements LongTaskConfiguration {
-	cleanupDelay: 60000;
 	maximumConcurrency: 2;
+	cleanupDelayInSeconds: 60;
+	maximumProcessingTimeInSeconds: 60;
 }
